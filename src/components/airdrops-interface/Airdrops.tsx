99e16b58fcd959a3e9f5578ae68635b4e5f7bd86
Prakriti Malik <prakriti.malik@gmail.com>
2022-08-31 21:41:07 +0530

    changes for airdrops

--- src/components/airdrops-interface/Airdrops.tsx
+++ src/components/airdrops-interface/Airdrops.tsx
@@ -1,5 +1,100 @@
 import React from 'react';
+import './Airdrops.css';
 
 export default function Airdrop() {
-    return <h1>Airdrop </h1> 
+    
+    return (
+        <>
+                   <div className="airdrops-interface row mx-0">
+        <div className='col-2 stepper'></div>
+        <div className="content col-10">
+        <h2 className="mt-2"> My Missions </h2>
+<div>
+  <div className="mission">
+    <h4> Mission 1 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Initial claim action </p>
+    <button> CLAIM </button>
+    </div>
+  </div>
+    <div className="mission">
+    <h4> Mission 2 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Deposit tier 1 (e.g. &gt; 5% of base_value) </p>
+    <button> CLAIM </button>
+    </div>
+  </div>
+    <div className="mission">
+    <h4> Mission 3 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Deposit tier 2 (e.g. &gt; 10% of base_value) </p>
+    <button> CLAIM </button>
+    </div>
+  </div>
+    <div className="mission">
+    <h4> Mission 4 </h4>
+    <div className="d-flex justify-content-between">
+    <p>  Deposit tier 2 (e.g. &gt; 15% of base_value) </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 5 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Deposit tier 3 (e.g. &gt; 22% of base_value) </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 6 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Deposit tier 4 (e.g. &gt; 30% of base_value) </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 7 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Active QCK delegation </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 8 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Intent is set </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 9 </h4>
+    <div className="d-flex justify-content-between">
+    <p> Cast governance vote on QS </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 10</h4>
+    <div className="d-flex justify-content-between">
+    <p> Governance By Proxy (GbP): cast vote on remote zone </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    <div className="mission">
+    <h4> Mission 11</h4>
+    <div className="d-flex justify-content-between">
+    <p> Provide liquidity on Osmosis </p>
+    <button> CLAIM </button>
+    </div>
+    </div>
+    
+     </div>
+  </div>
+</div>
+
+
+</>
+
+
+    )
 }
