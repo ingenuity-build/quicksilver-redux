549600ccd786d5562ee79acc69c87a95dae1c8e4
prakritimalik <prakriti.malik@gmail.com>
2023-01-23 13:38:21 +0530

    update staking successful message

--- src/components/staking-interface/delegate/CongratulationsPane.tsx
+++ src/components/staking-interface/delegate/CongratulationsPane.tsx
@@ -22,7 +22,7 @@ export default function CongratulationsPane() {
     return (
         <>
         <div className="congratulations-pane d-flex mt-5 justify-content-center align-items-center flex-column">
-    <h4 className="mt-5">Your assets have been successfully staked!</h4> 
+    <h4 className="mt-5">Your assets have been successfully staked! The qAssets will arrive in your Quicksilver wallet shortly.</h4> 
     <div className="button-container mt-4">
         <button onClick={stakeAnotherNetwork} className="stake mx-2"> Stake on another network </button>
         </div>
