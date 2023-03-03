0a82793c8188dd75a07c19deb644e1a5d6c15ad2
prakritimalik <prakriti.malik@gmail.com>
2023-03-03 15:07:55 +0530

    changes for cosmostation

--- src/components/connect-wallet-modal/ConnectWalletModal.tsx
+++ src/components/connect-wallet-modal/ConnectWalletModal.tsx
@@ -31,6 +31,14 @@ localStorage.setItem( 'WalletType', 'leap' );
 
 }
 
+const connectCosmostation = () => {
+    // @ts-expect-error
+    dispatch(setWalletType('cosmostation'));
+    localStorage.setItem( 'WalletType', 'cosmostation' );
+    // @ts-expect-error
+    props?.handleClickOpen();
+}
+
 
     return (
         <>
@@ -40,7 +48,7 @@ localStorage.setItem( 'WalletType', 'leap' );
             <h4> {props.loading}</h4>
     <button className="m-2 p-3 keplr" disabled={props.loading} onClick={connectKeplr}>Keplr</button> 
     <button className="m-2 p-3 keplr" onClick={connectLeap}>Leap</button>
-    <button className="m-2 p-3 cosmos-station"  disabled> Cosmostation (Coming Soon) </button>
+    <button className="m-2 p-3 keplr" onClick={connectCosmostation} > Cosmostation </button>
     {props.loading && <h5 className="text-center mt-2"> Connecting...</h5>}
     </div>
     </div> 
