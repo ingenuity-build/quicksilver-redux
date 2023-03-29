9ee5f859f03c50c34b92017be2ddfa4496798865
prakritimalik <prakriti.malik@gmail.com>
2023-03-29 13:09:52 +0530

    changes for osmo

--- src/components/staking-interface/delegate/NetworkSelection.tsx
+++ src/components/staking-interface/delegate/NetworkSelection.tsx
@@ -4,7 +4,7 @@ import React , { useEffect, useState } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import './NetworkSelection.css';
 
-import {increaseStakingStep} from '../../../slices/stakingActiveStep';
+import {increaseStakingStep, setStakingStep} from '../../../slices/stakingActiveStep';
 
 
 import { networksSelector, fetchNetworks } from '../../../slices/networks'	;
@@ -12,7 +12,7 @@ import { selectedNetworkSelector, setSelectedNetwork, setSelectedNetworkFunc } f
 import { setNetworkAddress,  setNetworkWallet, setNetworkBalance, selectedNetworkWalletSelector, setClient } from "../../../slices/selectedNetworkWallet";
 import {quicksilverSelector} from '../../../slices/quicksilver';
 import { validatorListSelector } from "../../../slices/validatorList";
-
+import Moment from 'moment';
 
 
 export default function NetworkSelection() {
@@ -27,7 +27,7 @@ export default function NetworkSelection() {
   const initialText = 'Copy';
   const [buttonText, setButtonText] = useState(initialText);
   const [loadingValidators, setLoadingValidators] = useState(true);
-
+  const [showOsmoMessage, setShowOsmoMessage] = React.useState(false);
   function handleClick() {
    navigator.clipboard.writeText(networkAddress)
    setButtonText('Address copied');
@@ -49,6 +49,25 @@ export default function NetworkSelection() {
     }
 
 }, [balances, selectedNetwork])
+
+useEffect(() => {
+  if(selectedNetwork.base_denom === "uosmo") {
+  let format = 'HH:mm';
+
+let time = Moment.utc().format(format);
+let beforeTime = Moment('17:00', format);
+  let afterTime = Moment('17:30', format);
+if (Moment(time, 'HH:mm').isBetween(beforeTime, afterTime)) {
+  setShowOsmoMessage(true);
+
+} else {
+  setShowOsmoMessage(false);
+}
+}
+ else {
+  setShowOsmoMessage(false);
+ }
+}, [selectedNetwork])
 
 useEffect(() => {
   if(validatorList.length > 0) {
@@ -98,6 +117,13 @@ useEffect(() => {
     dispatch(increaseStakingStep());
   }
 
+  const onBack = () => {
+                 // @ts-expect-error
+                 dispatch(setStakingStep(1));
+                 // @ts-expect-error
+                 dispatch(setSelectedNetworkFunc("Select a network"));
+  }
+
 
     return (
         <>
@@ -111,7 +137,7 @@ useEffect(() => {
 </p> 
 </div>}
 {hasErrors && <p> There's an issue with fetching the network list. Please try again.</p>}
-{selectedNetwork !== "Select a network" && networkAddress !== '' && <div className="wallet-details d-flex flex-column mt-5">
+{selectedNetwork !== "Select a network" && !showOsmoMessage && networkAddress !== '' && <div className="wallet-details d-flex flex-column mt-5">
   <h4 className="mt-3"> My Wallet</h4>
   {networkAddress && <h6 className="mt-3"> {networkAddress} <button className="mx-2 copy-button"
   onClick={handleClick}
@@ -134,16 +160,19 @@ useEffect(() => {
 </div>}
 
 
-<div className="mt-5 button-container">
+{!showOsmoMessage && <div className="mt-5 button-container">
 {selectedNetwork !== "Select a network" && networkAddress !== '' &&   <p className={loadingValidators ? 'visible text-center' : 'invisible'}> Loading Validators...</p>}
  {selectedNetwork?.base_denom && <button className={`stake-liquid-atoms-button mx-3 ${selectedNetwork === "Select a network" ? 'd-none' : ''}`} onClick={() => onNext()} > Stake {selectedNetwork?.base_denom?.slice(1).toUpperCase()} </button>}
   {selectedNetwork.liquidity_module  && <button className={`stake-existing-delegations-button mx-3 ${selectedNetwork === "Select a network" ? 'd-none' : ''}`} > Stake Existing Delegations </button>}
 
+</div>}
+{showOsmoMessage && <h5 className="mt-5 w-50 text-center m-auto">OSMO deposits are disabled due to congestion on the Osmosis Network during the Osmosis epoch boundary from 17:00 - 17:30 UTC.</h5>}
+<div className="button-container">
+{showOsmoMessage && <button onClick={onBack} className="prev-button mx-3 mt-5" > Back</button>}
 </div>
 {/* {!selectedNetwork.liquidity_module && <p className={`mt-4 ${selectedNetwork === "Select a network" ? 'd-none' : ''}`}> Transfer of delegation isn't enabled on this network </p>} */}
 
 </div>
-
 
     </>
 
