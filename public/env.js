6462386051a4186e4bd6485cbfb10bca3a880dc6
prakritimalik <prakriti.malik@gmail.com>
2023-04-11 12:21:05 +0530

    update stake button text

--- public/env.js
+++ public/env.js
@@ -1,9 +1,9 @@
 window.env = {
-  "NODE_ENV": "preprod",
-  "QUICKSILVER_API": "https://lcd.quicksilver.zone",
-  "ZONE_URL": "quicksilver.zone",
+  "NODE_ENV": "test",
+  "QUICKSILVER_API": "https://lcd.test.quicksilver.zone",
+  "ZONE_URL": "test.quicksilver.zone",
   "REACT_APP_ENABLE_SET_INTENT": "false",
   "REACT_APP_ENABLE_UNBONDING": "false",
   "REACT_APP_ENABLE_CLAIMS": "true",
-  "WHITELISTED_ZONES": "cosmoshub-4,osmosis-1,regen-1,stargaze-1"
+  "WHITELISTED_ZONES": "uni-6,elgafar-1,osmo-test-4,regen-redwood-1,theta-testnet-001"
 };
