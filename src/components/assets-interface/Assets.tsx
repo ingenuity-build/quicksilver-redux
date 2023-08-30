55ecb86f882772ff6dada3318e077411122b086e
muku314115 <mukangotan@gmail.com>
2023-08-30 13:57:40 +0530

    adjusting gas as per master

--- src/components/assets-interface/Assets.tsx
+++ src/components/assets-interface/Assets.tsx
@@ -178,7 +178,7 @@ dispatch(setPoolModalOpen());
     quicksilverAddress,
       [...msg],
      {
-        "gas": (50000 + ((messages.length) * 50000)).toString(),
+        "gas": "400000",
         "amount": [
           {
             "denom": "uqck",
